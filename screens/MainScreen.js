194abecbe47d2475bfc997a83992f3de654b64ba
kimlg2015@naver,com <kimlg2015@naver.com>
2024-09-06 00:59:38 +0900

    Todo App구조 생성

---
+++ screens/MainScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MainScreen = () => {
+  return (
+    <View>
+    <Text></Text>
+    </View>
+  )
+}
+
+export default MainScreen
+
+const styles = StyleSheet.create({})
+
+
+
+
