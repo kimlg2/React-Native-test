5f124a2782d78c0d474e49b83fa72dc6caad1f95
kimlg2015@naver,com <kimlg2015@naver.com>
2024-09-16 16:11:48 +0900

    Todo App을

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,18 +1,19 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { StatusBar } from 'expo-status-bar'
+import { Platform } from 'react-native';
 
 const MainScreen = () => {
   return (
-   <SafeAreaView>
+   <SafeAreaView style={styles.container}>
     <StatusBar barStyle={'defailt'} />
-      <Text>app 소개</Text>
-      <View>
-        <Text>할 일</Text>
+      <Text style={styles.pageTitle}>app 소개</Text>
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>할 일</Text>
       </View>
-      <View />
-      <View>
-        <Text>완료 된일</Text>
+      <View style={styles.separator} />
+      <View style={styles.listView}>
+        <Text style={styles.listTitle}>완료 된 일</Text>
       </View>
     </SafeAreaView>
   )
@@ -20,4 +21,38 @@ const MainScreen = () => {
 
 export default MainScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? 20 : 0,
+        backgroundColor: '#f7f8fa'
+
+
+    },
+    pageTitle: {
+        marginBottom: 35,
+        paddingHorizontal: 15,
+        fintSize: 54,
+        fontWeight: '600'
+
+    },
+    separator :{ 
+        marginHorizontal: 10,
+        marginTop: 25,
+        marginBottom: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: 'raba(0,0,0,0,2)'
+
+    },
+    listView : {
+        flex: 1,
+
+        
+    },
+    listTitle: {
+        marginBottom: 25,
+        paddingHorizontal: 15,
+        fontSize: 41,
+        fontWeight: '500'
+    }
+})
