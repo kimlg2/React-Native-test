c23baee0fe8d5e4b308638e26b6f75b1916c6c55
kimlg2015@naver,com <kimlg2015@naver.com>
2024-09-18 02:33:13 +0900

    Value State 생성

--- components/inputForm.js
+++ components/inputForm.js
@@ -1,7 +1,18 @@
 import { Platform,KeyboardAvoidingView, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
 import React from 'react'
+import { addTodo } from '../redux/slices/todoSlice';
+import { useDispatch } from 'react-redux';
 
 const InputForm = () => {
+    const [currentValue, setCurrentValue] = useState("");
+    const dispatch = useDispatch();
+   
+     const handleSubmit = () => {
+        if(currentValue !=='') {
+            dispatch(addTodo(currentValue));
+            setCurrentValue('');
+        }
+    }
   return (
     <KeyboardAvoidingView
     behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
@@ -9,8 +20,11 @@ const InputForm = () => {
      <TextInput
      style={styles.inputField}
      placeholder='할 일을 작성해주세요.' 
+     value={currentValue}
+     onChangeText={setCurrentValue}
+     onSubmitEditing={handleSubmit}
      />
-     <Pressable style={styles.addButton}>
+     <Pressable style={styles.addButton} onPress={handleSubmit}>
         <Text style={styles.addButtonText}>+</Text>
      </Pressable>
 
