0874982bc4d19db6d904da680d4ae792138830ad
kimlg2015@naver,com <kimlg2015@naver.com>
2024-09-17 17:05:36 +0900

    SVG파일 삽입 에러 해결

--- components/Todoltem.js
+++ components/Todoltem.js
@@ -2,8 +2,8 @@ import { Pressable, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import CheckboxUnchecked from '../assets/checkbox-unchecked.svg';
 import CheckboxChecked from '../assets/checkbox-checked.svg';
+// import DeleteIcon from '../assets/delete.svg';
 import DeleteIcon from '../assets/delete.svg';
-
 const Todoltem = () => {
   return (
     <View style={styles.itemContainer}>
