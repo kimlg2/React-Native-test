a8fe6ec5eb7e3152a19f9c419ee0c712be46cd03
kimlg2015@naver,com <kimlg2015@naver.com>
2024-09-18 18:33:15 +0900

    store 생성

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import todoReducer from './slices/todo/todoSlice';
+import todoReducer from '../redux/slices/todoSlice.js';
 export const store = configureStore({
     reducer: {
         todo: todoReducer,
     }
 });
+
