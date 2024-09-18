3b3bc1a274dd7545b3788919cae3eaaabb450fe3
kimlg2015@naver,com <kimlg2015@naver.com>
2024-09-18 18:41:29 +0900

    currentValue가 빈 문자열이 아닌 경우, dispatch(addTodo(currentValue))를 통해 addTodo 액션을 전송하여 새로운 할 일을 추가

--- components/inputForm.js
+++ components/inputForm.js
@@ -1,5 +1,5 @@
 import { Platform,KeyboardAvoidingView, Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
-import React from 'react'
+import React,{useState} from 'react'
 import { addTodo } from '../redux/slices/todoSlice';
 import { useDispatch } from 'react-redux';
 
