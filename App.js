4e32f50a8bc4cbfa2f69e6dfff45981cfe16b29f
kimlg2015@naver,com <kimlg2015@naver.com>
2024-09-25 03:38:15 +0900

    Toast 연동

--- App.js
+++ App.js
@@ -7,20 +7,25 @@ import { NavigationContainer } from '@react-navigation/native';
 import LoginScreen from './screens/LoginScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import app from './firebase';
+import { Toast } from 'react-native-toast-message/lib/src/Toast';
 export default function App() {
   
   const Stack = createNativeStackNavigator();
   return (
-    <Provider store={store}>
+    <>
+        <Provider store={store}>
 
-   <NavigationContainer>
-    <Stack.Navigator>
-    <Stack.Screen options={{headerShown:false}} name="Login" component={LoginScreen} />
-      <Stack.Screen options={{headerShown:false}} name="Main" component={MainScreen} />
-    </Stack.Navigator>
-     </NavigationContainer>
-    
-     </Provider>
+<NavigationContainer>
+ <Stack.Navigator>
+ <Stack.Screen options={{headerShown:false}} name="Login" component={LoginScreen} />
+   <Stack.Screen options={{headerShown:false}} name="Main" component={MainScreen} />
+ </Stack.Navigator>
+  </NavigationContainer>
+ 
+  </Provider>
+  <Toast/>
+    </>
+
 
   );
 }
