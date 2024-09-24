f7a3c4e9750d052a5524c12966025693e870ce61
kimlg2015@naver,com <kimlg2015@naver.com>
2024-09-25 04:07:48 +0900

    firebase를 활용한 로그인 기능 구현

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,25 @@
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ListIcon from '../assets/list.svg';
-import { getAuth,createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth,createUserWithEmailAndPassword, signInWithEmailAndPassword,onAuthStateChanged } from 'firebase/auth';
 import Toast from 'react-native-toast-message';
+import { useNavigation } from '@react-navigation/native';
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const navigation = useNavigation();
   const auth = getAuth();
+  
+  useEffect(() => {
+    onAuthStateChanged(auth,user => {
+      console.log('onAuthStateChanged user', user);
+      if(user){
+              navigation.replace('Main');
+            }
+    }); 
+
+
+  },[])
   const handleSignUp = async () => {
     try {
     const user = await createUserWithEmailAndPassword(auth,email,password);
@@ -27,8 +40,18 @@ const LoginScreen = () => {
   }
 }
   const handleLogin = async () => {
-   
+    try {
+  const user = await signInWithEmailAndPassword(auth,email,password);
+  console.log(user);
+    } catch (error) {
+      Alert.alert (
+        "로그인 도중에 문제가 발생!",
+        error.message,
+        [{text: '닫기', onPress: () => console.log('닫기')}],
+        {cancelable: true}
+        )
   }
+}
   return (
     <View
     style={styles.container}
